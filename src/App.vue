51dc01b397dd720944a8cb0db5a1f0137e76577a
Niklas Grieger <niklas.grieger@devnik.dev>
2024-03-27 22:52:36 +0800

    add vercel analytics

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <script setup>
 import AppBar from './components/layout/AppBar.vue'
 import { SpeedInsights } from "@vercel/speed-insights/vue"
+
+import { inject } from '@vercel/analytics';
+inject();
 </script>
 <template>
   <AppBar class="mb-6"></AppBar>
--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,6 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-import { inject } from '@vercel/analytics';
-inject();
 const app = createApp(App)
 
 app.use(createPinia())
