3ff436fca9e8772682fff041b23426b749a6fd69
Niklas Grieger <niklas.grieger@devnik.dev>
2024-03-27 21:07:34 +0800

    add vercel speed insight

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup>
 import AppBar from './components/layout/AppBar.vue'
+import { SpeedInsights } from "@vercel/speed-insights/vue"
 </script>
 <template>
   <AppBar class="mb-6"></AppBar>
   <router-view />
+  <SpeedInsights />
 </template>
