3a3370ea0ba70052101df903e885c6dc0291f5dd
Niklas Grieger <niklas.grieger@devnik.dev>
2024-07-03 00:18:39 +0800

    try another seo text

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,7 @@ useHead({
   meta: [
     {
       name: 'description',
-      content: "Hello my name is Niklas Grieger and I am a passionate full stack developer specialized in VueJs and Nuxt. My latest tech stack: VueJs 2 and 3, Nuxt, Vuetify, CI/CD with Gitlab / Bitbucket, PostgreSQL and AWS. Feel free to contact me if you need a Vue 2 to Vue3 migration or any other topics.",
+      content: "My service: Freelance full stack development, Vue3 migration, Vue, Technical Consultation, Javascript Expert, Individual support",
     },
   ],
 })
